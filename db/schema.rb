de55543d1d0222620f45ef6326c33799904fb714
Mariana Maia <code@mariana.dk>
2021-04-30 02:43:26 +0200

    added sales model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_000518) do
+ActiveRecord::Schema.define(version: 2021_04_30_003714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2021_04_30_000518) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_sales_on_product_id"
+    t.index ["user_id"], name: "index_sales_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +51,6 @@ ActiveRecord::Schema.define(version: 2021_04_30_000518) do
   end
 
   add_foreign_key "products", "users"
+  add_foreign_key "sales", "products"
+  add_foreign_key "sales", "users"
 end
