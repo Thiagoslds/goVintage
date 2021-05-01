b2e66b59f49f31717b6dff8f95cf170a3567b3c8
Mariana Maia <code@mariana.dk>
2021-05-01 20:48:20 +0200

    sales models and views

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,2 +1,30 @@
 class SalesController < ApplicationController
+
+  before_action :set_sale, only: [:show]
+
+  def new
+    @sale = Sale.new
+  end
+
+  def show
+  end
+
+  def create
+    # @buyer = current_user
+    @sale = Sale.new
+    @product = Product.find(params[:product_id])
+    @sale.user = current_user
+    @sale.product = @product
+    if @sale.save
+      redirect_to sale_path(@sale)
+    end
+
+  end
+
+  private
+
+  def set_sale
+    @sale = Sale.find(params[:id])
+  end
+
 end
