ad5f18c0935a31ef6016304d7c07dd8591746e4a
Thiago Silveira <thiagoslds@hotmail.com>
2021-05-04 22:06:09 -0300

    atualizando autorizações para os produtos e retirando opção de comprar de si mesmo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,27 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all
+    @products = policy_scope(Product).order(created_at: :desc)
+    #@products = Product.all
   end
 
   def new
     @product = Product.new
+    authorize @product
   end
 
   def show
     @product = Product.find(params[:id])
+    authorize @product
   end
 
   def create
-    authorize @product
     # associar o produto ao user
     @product = Product.new(product_params)
     # associar o produto ao user
     @product.user = current_user
+    authorize @product
+
     if @product.save
       redirect_to product_path(@product)
     else
@@ -27,15 +31,18 @@ class ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+    authorize @product
   end
 
   def update
     @product = Product.find(params[:id])
     @product.update(product_params)
+    authorize @product
     redirect_to product_path(@product)
   end
 
   def destroy
+    authorize @product
     @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_path
--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -1,5 +1,25 @@
 class ProductPolicy < ApplicationPolicy
   class Scope < Scope
+    def update?
+      record.user == user
+    end
+
+    def destroy?
+      record.user == user
+    end
+
+    def edit?
+      record.user == user
+    end
+
+    def show?
+      true
+    end
+
+    def new?
+      true
+    end
+
     def create?
       return true
     end
