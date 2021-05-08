f1dac4c3f8932aa1141fcd7fe58ad7cc5b8f410b
Lorena Marinho <lor.marinho@gmail.com>
2021-05-08 15:21:45 -0300

    fixed bugs

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2021_05_08_144602) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.boolean "sold"
+    t.boolean "sold", default: false
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2021_05_08_144602) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "sold"
     t.index ["product_id"], name: "index_sales_on_product_id"
     t.index ["user_id"], name: "index_sales_on_user_id"
   end
